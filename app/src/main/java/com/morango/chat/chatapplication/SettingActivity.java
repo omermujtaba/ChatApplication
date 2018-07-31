5c6359a75da208aba626c3c3654a9b2e635a1505
omermujtaba <omer_mujtaba@hotmail.com>
2018-07-31 13:33:03 +0500

    Commit 6 1:32

--- app/src/main/java/com/morango/chat/chatapplication/SettingActivity.java
+++ app/src/main/java/com/morango/chat/chatapplication/SettingActivity.java
@@ -1,12 +1,32 @@
 package com.morango.chat.chatapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -16,8 +36,11 @@ public class SettingActivity extends AppCompatActivity {
     TextView userStatus, userName;
 
     Button changStatus, changeImage;
-
     Toolbar toolbar;
+
+    FirebaseAuth mAuth;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +53,24 @@ public class SettingActivity extends AppCompatActivity {
         changStatus = findViewById(R.id.changeStatusButton);
         changeImage = findViewById(R.id.changeImageButton);
         toolbar = findViewById(R.id.settingToolBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User").child(mAuth.getCurrentUser().getUid());
+        storageReference = FirebaseStorage.getInstance().getReference().child("ProfilePictures");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userName.setText(dataSnapshot.child("userName").getValue().toString());
+                userStatus.setText(dataSnapshot.child("userStatus").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Settings");
@@ -38,6 +79,8 @@ public class SettingActivity extends AppCompatActivity {
         changeImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                CropTheImage();
 
             }
         });
@@ -50,6 +93,72 @@ public class SettingActivity extends AppCompatActivity {
                 startActivity(in);
             }
         });
+
+    }
+
+    private void CropTheImage() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setActivityTitle("Crop")
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .setAspectRatio(1, 1)
+                .setMaxCropResultSize(1000, 1000)
+                .setMinCropWindowSize(400, 400)
+                .start(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+
+                Picasso.get().load(result.getUri()).error(R.drawable.default_profile).into(userImage);
+
+//                storageReference.child(mAuth.getCurrentUser().getUid() + ".jpg").putFile(result.getUri())
+//                        .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    Toast.makeText(getApplicationContext(), "Saving to database: Success", Toast.LENGTH_LONG).show();
+//                                } else {
+//                                    Toast.makeText(getApplicationContext(), "Saving to database: Failed: " + task.getException().toString(), Toast.LENGTH_LONG).show();
+//                                }
+//                            }
+//                        });
+
+
+                final UploadTask uploadTask = storageReference.child(mAuth.getCurrentUser().getUid() + ".jpg").putFile(result.getUri());
+
+                Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        return storageReference.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri uri = task.getResult();
+                            Toast.makeText(getApplicationContext(), "Saving to database: success: " + uri.toString(), Toast.LENGTH_LONG).show();
+//                            databaseReference.child("userImage").setValue(uri.toString());
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Saving to database: Failed: " + task.getException().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+            }
+        }
 
     }
 }
